ab5589a8735f34e4b01509274843f6f85da1c91f
lucy <232277726+lucyarmstrong-schmingus@users.noreply.github.com>
2025-12-09 10:13:40 +0800

    bingus

--- highscore/databaseTime.py
+++ highscore/databaseTime.py
@@ -7,8 +7,14 @@ curse = batadase.cursor()
 scorey = sys.argv[1] #score
 entry = sys.argv[2] #name
 
-argument = f"INSERT INTO scores (score, name) VALUES({scorey}, {entry})"
-curse.execute(argument)
+addArgument = f"INSERT INTO scores (score, name) VALUES({scorey}, '{entry}');"
+removeArgument = "DELETE FROM scores WHERE MIN(score);"
+
+curse.execute(addArgument)
+
+if int(curse.execute("SELECT COUNT(*) FROM scores;")) >100:
+    curse.execute(removeArgument)
+
 
 curse.commit()
 curse.close()
